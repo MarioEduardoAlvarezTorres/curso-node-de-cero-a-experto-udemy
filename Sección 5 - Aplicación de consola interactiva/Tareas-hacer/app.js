8e706b1e614380c7417b2e9b9ed794f93d5b0da7
MarioEduardo <eduardoat44@gmail.com>
2022-06-21 18:04:21 -0500

    Seccion 5: Repetir de forma infinita un menu video 47

--- Sección 5 - Aplicación de consola interactiva/Tareas-hacer/app.js
+++ Sección 5 - Aplicación de consola interactiva/Tareas-hacer/app.js
@@ -2,9 +2,12 @@ require('colors');
 const { mostrarMenu, pausa } = require('./helpers/mensajes');
 console.clear();
 const main = async() =>{
-    console.log("Hola mundo");
-    mostrarMenu();
-    pausa();
+    //console.log("Hola mundo");
+    let opt = '';
+    do {
+        opt= await mostrarMenu();
+        await pausa();
+    } while (opt != '0');
 }
 
 main();
--- Sección 5 - Aplicación de consola interactiva/Tareas-hacer/helpers/mensajes.js
+++ Sección 5 - Aplicación de consola interactiva/Tareas-hacer/helpers/mensajes.js
@@ -1,39 +1,46 @@
+const { resolve } = require('path');
+
 require('colors');
 
 const mostrarMenu = () =>{
-    console.clear();
-    console.log("=====================".magenta);
-    console.log("Seleccione una opción".magenta);
-    console.log("=====================\n".magenta);
+    return new Promise(resolve =>{
+        console.clear();
+        console.log("=====================".magenta);
+        console.log("Seleccione una opción".magenta);
+        console.log("=====================\n".magenta);
 
-    console.log(`${'1.'.magenta}Crear una tareas`);
-    console.log(`${'2.'.magenta}Listar tareas`);
-    console.log(`${'3.'.magenta}Listar tareas completadas`);
-    console.log(`${'4.'.magenta}Listar tareas pendientes`);
-    console.log(`${'5.'.magenta}Completar tarea(s)`);
-    console.log(`${'6.'.magenta}Borrar tarea`);
-    console.log(`${'0.'.magenta}Salir`);
+        console.log(`${'1.'.magenta}Crear una tareas`);
+        console.log(`${'2.'.magenta}Listar tareas`);
+        console.log(`${'3.'.magenta}Listar tareas completadas`);
+        console.log(`${'4.'.magenta}Listar tareas pendientes`);
+        console.log(`${'5.'.magenta}Completar tarea(s)`);
+        console.log(`${'6.'.magenta}Borrar tarea`);
+        console.log(`${'0.'.magenta}Salir`);
 
-    const readline = require('readline').createInterface({
-        input : process.stdin,
-        output: process.stdout
+        const readline = require('readline').createInterface({
+            input : process.stdin,
+            output: process.stdout
+        });
+
+        readline.question("Selecciona una opción: ",(opt) =>{
+            readline.close();
+            resolve(opt);
+        });
     });
-
-    readline.question("Selecciona una opción: ",(opt) =>{
-        readline.close();
-    });
-
 }
 
 const pausa = () =>{
-    const readline = require('readline').createInterface({
-        input : process.stdin,
-        output: process.stdout
-    });
-
-    readline.question(`\n Presionar ${'Enter'.blue} para continuar \n`,(opt) =>{
-        readline.close();
-    });
+    return new Promise(resolve => {
+        const readline = require('readline').createInterface({
+            input : process.stdin,
+            output: process.stdout
+        });
+    
+        readline.question(`\n Presionar ${'Enter'.blue} para continuar \n`,(opt) =>{
+            readline.close();
+            resolve();
+        });
+    })
 }
 
 module.exports = {

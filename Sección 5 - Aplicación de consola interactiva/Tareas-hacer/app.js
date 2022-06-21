14f307c63a17c90cc1dbd6e74d64e405a5ba2ac1
MarioEduardo <eduardoat44@gmail.com>
2022-06-21 16:49:26 -0500

    Seccion 5: stdin-stdout - readline video 46

--- Sección 5 - Aplicación de consola interactiva/Tareas-hacer/app.js
+++ Sección 5 - Aplicación de consola interactiva/Tareas-hacer/app.js
@@ -1,7 +1,10 @@
 require('colors');
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
 console.clear();
 const main = async() =>{
     console.log("Hola mundo");
+    mostrarMenu();
+    pausa();
 }
 
 main();
---
+++ Sección 5 - Aplicación de consola interactiva/Tareas-hacer/helpers/mensajes.js
@@ -0,0 +1,42 @@
+require('colors');
+
+const mostrarMenu = () =>{
+    console.clear();
+    console.log("=====================".magenta);
+    console.log("Seleccione una opción".magenta);
+    console.log("=====================\n".magenta);
+
+    console.log(`${'1.'.magenta}Crear una tareas`);
+    console.log(`${'2.'.magenta}Listar tareas`);
+    console.log(`${'3.'.magenta}Listar tareas completadas`);
+    console.log(`${'4.'.magenta}Listar tareas pendientes`);
+    console.log(`${'5.'.magenta}Completar tarea(s)`);
+    console.log(`${'6.'.magenta}Borrar tarea`);
+    console.log(`${'0.'.magenta}Salir`);
+
+    const readline = require('readline').createInterface({
+        input : process.stdin,
+        output: process.stdout
+    });
+
+    readline.question("Selecciona una opción: ",(opt) =>{
+        readline.close();
+    });
+
+}
+
+const pausa = () =>{
+    const readline = require('readline').createInterface({
+        input : process.stdin,
+        output: process.stdout
+    });
+
+    readline.question(`\n Presionar ${'Enter'.blue} para continuar \n`,(opt) =>{
+        readline.close();
+    });
+}
+
+module.exports = {
+    mostrarMenu,
+    pausa
+}

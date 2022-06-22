c7eb79751b50285dced9ea9d8ae81240c108a1af
MarioEduardo <eduardoat44@gmail.com>
2022-06-22 16:28:08 -0500

    Seccion 5: Opciones del menu interactivo video 50

--- Sección 5 - Aplicación de consola interactiva/Tareas-hacer/app.js
+++ Sección 5 - Aplicación de consola interactiva/Tareas-hacer/app.js
@@ -1,11 +1,13 @@
 import colors from 'colors';
-import { inquirerMenu } from './helpers/inquirer.js';
+import { inquirerMenu, pausa } from './helpers/inquirer.js';
 console.clear();
 const main = async() =>{
     //console.log("Hola mundo");
     let opt = '';
     do {
         opt= await inquirerMenu();
+        console.log({opt})
+        await pausa();
     } while (opt != '0');
 }
 
--- Sección 5 - Aplicación de consola interactiva/Tareas-hacer/helpers/inquirer.js
+++ Sección 5 - Aplicación de consola interactiva/Tareas-hacer/helpers/inquirer.js
@@ -6,17 +6,43 @@ const preguntas = [
         type:'list',
         name: 'opcion',
         message: '¿Qué deseas hacer?',
-        choices:['opt1','opt2','opt3']
+        choices:[
+            {value:'1',
+             name:"1.Crear tarea"},
+            {value:'2',
+             name:"2.Lista de tareas"},
+            {value:'3',
+             name:"3.Vistar Tareas completadas"},
+            {value:'4',
+             name:"4.Listar tareas pendientes"},
+            {value:'5',
+             name:"5.Completar tarea(s)"},
+            {value:'6',
+             name:"6.Borrar Tarea"},
+            {value:'0',
+             name:"0.Salir"}
+        ]
     }
 ];
+
+const siguiente = [{
+    type:'input',
+    name:'siguiente',
+    message:`Presiones ${'Enter'.magenta} para continuar`,
+}];
 const inquirerMenu = async() =>{
     console.clear();
     console.log("=====================".magenta);
     console.log("Seleccione una opción".magenta);
     console.log("=====================\n".magenta);
 
-    const opt= await inquirer.prompt(preguntas);
-    return opt;
+    const {opcion} = await inquirer.prompt(preguntas);
+    return opcion;
 }   
 
-export {inquirerMenu}
+const pausa = async() =>{
+    await inquirer.prompt(siguiente);
+    console.log("Estamos en Pausa")
+    console.log("\n")
+}
+export {inquirerMenu,pausa}

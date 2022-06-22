91f11ca685c3a7157c0726c470198e9bfe717f83
MarioEduardo <eduardoat44@gmail.com>
2022-06-22 18:27:31 -0500

    Seccion 5: Logica para el manejo de tareas por hacer video 51

--- Sección 5 - Aplicación de consola interactiva/Tareas-hacer/app.js
+++ Sección 5 - Aplicación de consola interactiva/Tareas-hacer/app.js
@@ -1,12 +1,19 @@
 import colors from 'colors';
 import { inquirerMenu, pausa } from './helpers/inquirer.js';
+//import {Tarea} from './helpers/tarea.js';
+import  {Tarea} from './models/tarea.js';
+import  {Tareas} from './models/tareas.js';
 console.clear();
 const main = async() =>{
     //console.log("Hola mundo");
     let opt = '';
     do {
-        opt= await inquirerMenu();
-        console.log({opt})
+        // opt= await inquirerMenu();
+        // console.log({opt})
+        const tarea = new Tarea('Comparar comida');
+        const tareas = new Tareas();
+        tareas._listado[tarea.id] = tarea;
+        console.log(tareas);
         await pausa();
     } while (opt != '0');
 }
---
+++ Sección 5 - Aplicación de consola interactiva/Tareas-hacer/models/tareas.js
@@ -0,0 +1,9 @@
+import { v4 as uuidv4 } from 'uuid';
+class Tareas {
+    _listado = {}
+    constructor(){
+        this._listado={}
+    }
+}
+
+export{Tareas}

75bedf24768fc68af041ca6d94a26713e49e2485
MarioEduardo <eduardoat44@gmail.com>
2022-06-27 22:33:55 -0500

    Seccion 6:Menu de la aplicación video 68

--- Sección 6 - Aplicación del clima/Aplicacion-clima/helpers/inquirer.js
+++ Sección 6 - Aplicación del clima/Aplicacion-clima/helpers/inquirer.js
@@ -7,29 +7,21 @@ const preguntas = [
         name: 'opcion',
         message: '¿Qué deseas hacer?',
         choices:[
-            {value:'1',
-             name:`${'1.'.magenta}Crear tarea`},
-            {value:'2',
-             name:`${'2.'.magenta}Lista de tareas`},
-            {value:'3',
-             name:`${'3.'.magenta}Vistar Tareas completadas`},
-            {value:'4',
-             name:`${'4.'.magenta}Listar tareas pendientes`},
-            {value:'5',
-             name:`${'5.'.magenta}Completar tarea(s)`},
-            {value:'6',
-             name:`${'6.'.magenta}Borrar Tarea`},
-            {value:'0',
-             name:`${'7.'.magenta}Salir`}
+            {value:1,
+             name:`${'1.'.magenta} Buscar ciudad`},
+            {value:2,
+             name:`${'2.'.magenta} Historial`},
+            {value:0,
+             name:`${'0.'.magenta} Salir`},
         ]
     }
 ];
-
 const siguiente = [{
     type:'input',
     name:'siguiente',
     message:`Presiones ${'Enter'.magenta} para continuar`,
 }];
+
 const inquirerMenu = async() =>{
     console.clear();
     console.log("=====================".magenta);
@@ -42,8 +34,8 @@ const inquirerMenu = async() =>{
 
 const pausa = async() =>{
     await inquirer.prompt(siguiente);
-    console.log("Estamos en Pausa")
-    console.log("\n")
+    /*console.log("Estamos en Pausa")
+    console.log("\n")*/
 }
 
 const leerInput = async(message) =>{
--- Sección 6 - Aplicación del clima/Aplicacion-clima/index.js
+++ Sección 6 - Aplicación del clima/Aplicacion-clima/index.js
@@ -1,8 +1,27 @@
-import {leerInput} from './helpers/inquirer.js';
+import {leerInput,pausa,inquirerMenu} from './helpers/inquirer.js';
 
 const main = async()=>{
-    const texto = await leerInput('Hola:');
-    console.log(texto);
+    let opt;
+    do {
+        //IMPRIME EL MENU
+        opt= await inquirerMenu();
+        switch (opt) {
+            case 1: 
+                console.log("Estamos en el Uno");
+            break;
+                
+            case 2:
+                console.log("Estamos en el Dos");
+            break;
+            
+            case 0: 
+                console.log("Estamos en el Salir");
+            break;
+            default:
+                break;
+        }
+        await pausa();
+    } while (opt != 0);
 }
 
 main();

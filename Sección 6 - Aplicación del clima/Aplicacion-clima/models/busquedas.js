633d779ec44378376a502a4337c617b75cbb5a7c
MarioEduardo <eduardoat44@gmail.com>
2022-06-28 00:07:41 -0500

    Seccion 5:Mapbox Search API y Token video 73

--- Sección 6 - Aplicación del clima/Aplicacion-clima/models/busquedas.js
+++ Sección 6 - Aplicación del clima/Aplicacion-clima/models/busquedas.js
@@ -5,10 +5,18 @@ export class Busquedas {
         //TODO leer DB si existe
     }
     async ciudad(lugar = ''){
-        //peticion http
-        //console.log('Ciudad: ',lugar);
-        const resp = await axios.get('https://reqres.in/api/users?page=2')
-        console.log(resp.data);
+        try {
+            //peticion http
+            //console.log('Ciudad: ',lugar);
+            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/mexico.json?proximity=ip&types=place%2Cpostcode%2Caddress&language=es&access_token=pk.eyJ1IjoibWFyaW9lZHVhcmRvIiwiYSI6ImNsNHhvbGkyczFxdm8zZW9iMm12c3VoaGEifQ.G5VzUnnwlD9su-T4FgchpQ')
+            console.log(resp.data);
+
+            return [];
+        } catch (error) {
+            console.log("No se encontró nada")
+            return [];
+        }
+    
         return [];//regresa los lugares que coincida con la busqueda
     }
 }

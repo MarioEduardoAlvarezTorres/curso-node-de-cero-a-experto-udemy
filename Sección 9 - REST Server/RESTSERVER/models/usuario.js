37bde6bef90153396722d88df55fedc243f60abf
MarioEduardo <eduardoat44@gmail.com>
2022-07-07 22:41:48 -0500

    Seccion 9: Video 119 Modelo de Usuario

---
+++ Sección 9 - REST Server/RESTSERVER/models/usuario.js
@@ -0,0 +1,37 @@
+const {Schema,model} = require("mongoose");
+const { required, boolean } = require("yargs");
+const UsuarioSchema = Schema({
+    nombre:{
+        type: String,
+        required:[true,"El nombre es obligatorio"]
+    },
+    correo:{
+        type: String,
+        required:[true,"El correo es obligatorio"],
+        unique: true
+    },
+    password:{
+        type: String,
+        required:[true,"El password es obligatorio"]
+    },
+    img:{
+        type: String,
+    },
+    rol:{
+        type: String,
+        required:true,
+        emun:['ADMIN_ROLE','USER_ROLE']
+    },
+    estado:{
+        type: Boolean,
+        default:true
+    },
+    google:{
+        type:boolean,
+        default:false
+    }
+});
+
+
+
+module.exports= model('Usuario',UsuarioSchema)

d6941e56d01e378c0ab536710f6187be9ad5f26e
MarioEduardo <eduardoat44@gmail.com>
2022-07-11 17:34:46 -0500

    Seccion 9: Valida todos los campos necesarios - Video 123

--- Sección 9 - REST Server/RESTSERVER/controllers/usuarios.js
+++ Sección 9 - REST Server/RESTSERVER/controllers/usuarios.js
@@ -1,7 +1,7 @@
 const { response, request } = require('express');
 const bcryptjs = require('bcryptjs')
 const Usuario = require('../models/usuario');
-const { validationResult } = require('express-validator');
+
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -18,10 +18,7 @@ const usuariosGet = (req = request, res = response) => {
 }
 
 const usuariosPost = async (req, res = response) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json(errors);
-    }
+    
     const {nombre,correo,password,rol} = req.body;
     const usuario = new Usuario({nombre,correo,password,rol});
 
--- Sección 9 - REST Server/RESTSERVER/routes/usuarios.js
+++ Sección 9 - REST Server/RESTSERVER/routes/usuarios.js
@@ -7,6 +7,7 @@ const { usuariosGet,
         usuariosPost,
         usuariosDelete,
         usuariosPatch } = require('../controllers/usuarios');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
@@ -17,6 +18,10 @@ router.put('/:id', usuariosPut );
 
 router.post('/',[
     check('correo',"El correo no es valido").isEmail(),
+    check('nombre',"El nombre es obligatorio").not().isEmpty(),
+    check('password',"La contraseña es obligatoria y debe contar con más de 6 letras").not().isEmpty().isLength({min:6}),
+    check('rol',"No es un rol valido").isIn(['ADMIN_ROLE','USER_ROLE']),
+    validarCampos 
 ],usuariosPost );
 
 router.delete('/', usuariosDelete );

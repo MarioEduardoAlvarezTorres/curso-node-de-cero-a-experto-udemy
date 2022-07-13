25bc03654cdd0a4234fa67fb40c578cf4e82a6f4
MarioEduardo <eduardoat44@gmail.com>
2022-07-13 18:17:08 -0500

    Seccion 10:Login de Usuario - video 141

--- Sección 10 - Autenticación de usuario - JWT/RESTSERVER/controllers/auth.js
+++ Sección 10 - Autenticación de usuario - JWT/RESTSERVER/controllers/auth.js
@@ -1,11 +1,42 @@
 const { response } = require("express");
+const bcryptjs = require('bcryptjs')
+const Usuario = require("../models/usuario")
 
-const login = (req, res = response) =>{
+const login = async (req, res = response) =>{
+    const {correo,password} =req.body;
+    try {
+        //VERIFICAR SI EL EMAIL EXISTE
+        const usuario = await Usuario.findOne({correo});
+        if(!usuario){
+            return res.status(400).json({
+                msg:`Usuario / Password no son correctos - correo`
+            })
+        }
+        //VERIFICAR SI EL USUARIO ESTA ACTIVO
+        if(usuario.estado === false){
+            return res.status(400).json({
+                msg:`Usuario no activo`
+            })
+        }
+        //VERIFICAR LA CONTRASEÑA
+        const validPassword = bcryptjs.compareSync(password,usuario.password);
 
-    res.json({
-        msg:'Login ok'
-    })
-
+        if(!validPassword){
+            return res.status(400).json({
+                msg:'Usuario / Password no son correctos - password'
+            })
+        }
+        //GENERAR EL JWT
+        res.json({
+            msg:'Login ok',
+            correo,
+            password
+        }) 
+    } catch (error) {
+        return res.status(500).json({
+            msg:"Algo salió mal"
+        })
+    }
 }
 
 module.exports = {

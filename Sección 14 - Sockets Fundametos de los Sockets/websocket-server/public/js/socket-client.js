116520479b9ed1b460fee7e8bea3739265105faf
MarioEduardo <eduardoat44@gmail.com>
2022-08-23 13:56:01 -0500

    Seccion 15: Sockets - Aplicación de Cola

--- Sección 14 - Sockets Fundametos de los Sockets/websocket-server/public/js/socket-client.js
+++ Sección 14 - Sockets Fundametos de los Sockets/websocket-server/public/js/socket-client.js
@@ -1,12 +1,18 @@
+
 // Referencias del HTML
 const lblOnline  = document.querySelector('#lblOnline');
 const lblOffline = document.querySelector('#lblOffline');
 const txtMensaje = document.querySelector('#txtMensaje');
 const btnEnviar  = document.querySelector('#btnEnviar');
 
+
 const socket = io();
 
+
+
 socket.on('connect', () => {
+    // console.log('Conectado');
+
     lblOffline.style.display = 'none';
     lblOnline.style.display  = '';
 
